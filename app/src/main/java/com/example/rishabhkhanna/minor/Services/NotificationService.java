47432ec2dc0101b0e3c274a506da1f561ee92c0e
Rishabh Khanna <rishabhkhanna777@gmail.com>
2016-11-29 01:52:01 +0530

    firebase notification in background and foreground added

---
+++ app/src/main/java/com/example/rishabhkhanna/minor/Services/NotificationService.java
@@ -0,0 +1,74 @@
+package com.example.rishabhkhanna.minor.Services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.example.rishabhkhanna.minor.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class NotificationService extends FirebaseMessagingService{
+
+    private String TAG = "Service";
+
+    public NotificationService() {
+    }
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        Log.d("Data Service" , data.get("Rishabh"));
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, remoteMessage.getFrom());
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_account_circle_black_24dp)
+                        .setContentTitle(data.get("Rishabh"))
+                        .setContentText(data.get("price")).setDefaults(Notification.DEFAULT_SOUND);
+
+
+        NotificationManager mgr = (NotificationManager) getSystemService(NotificationService.NOTIFICATION_SERVICE);
+        mgr.notify(0 , mBuilder.build());
+
+
+
+
+
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+
+
+}
--- app/src/main/java/com/example/rishabhkhanna/minor/view/Activities/Seat_Book.java
+++ app/src/main/java/com/example/rishabhkhanna/minor/view/Activities/Seat_Book.java
@@ -3,34 +3,38 @@ package com.example.rishabhkhanna.minor.view.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.rishabhkhanna.minor.R;
+import com.example.rishabhkhanna.minor.Services.NotificationService;
 
 
 public class Seat_Book extends AppCompatActivity {
 
-    public static final String url = "localhost:3000";
+    public static final String url = "http://192.168.1.101:3000/webview";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat__book);
 
+
+
         Intent i = getIntent();
         int position = i.getIntExtra("position" , 0);
 
-        WebView webView = new WebView(Seat_Book.this);
+        WebView webView = (WebView) findViewById(R.id.seating_webview);
 
         WebSettings webSettings = webView.getSettings();
 
         webSettings.setJavaScriptEnabled(true);
-
+        Log.d("view" , url);
         webView.loadUrl(url);
 
-
-
+        Intent intent = new Intent(Seat_Book.this , NotificationService.class);
+        startService(intent);
 
     }
 }
